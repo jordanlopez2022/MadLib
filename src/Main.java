c1b1ee0b74023d2077cef1b402f77ed526cdb6bd
LOPEZ <jordan.lopez.2022@student.lhschools.org>
2020-02-25 13:55:16 -0500

    intellij commit

---
+++ src/Main.java
@@ -0,0 +1,123 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//    phase 1
+        String adjectiveWord1 = "large";
+        String adjectiveWord2 = "small";
+        String nounWord1 = "vegetable";
+        String nounWord2 = "food";
+        String pluralNounWord1 = "foods";
+        String gameWord1 = "Super Mario Bros";
+        String pluralNounWord2 = "Marios";
+        String verbEndingInINGWord1 = "making";
+        String verbEndingInINGWord2 = "placing";
+        String pluralNounWord3 = "notifications";
+        String verbEndingInINGWord3 = "building";
+        String nounWord3 = "fruit";
+        String plantWord = "tree";
+        String partOfTheBodyWord = "mouth";
+        String aPlaceWord = "school";
+        String verbEndingInINGWord4 = "wheeling";
+        String adjectiveWord3 = "orange";
+        String numberWord = "1";
+        String pluralNounWord4 = "kings";
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+//     phase 2
+        System.out.println("What's your favorite Food?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Size?");
+        adjectiveWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Size Is, "+ adjectiveWord1);
+
+        System.out.println("What's your favorite Size?");
+        adjectiveWord2 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ adjectiveWord2);
+
+        System.out.println("What's your favorite Food?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Plural?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Game?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Plural?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Verb?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Verb?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Plural?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Verb?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Food?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Tree?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Body Parts?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Place?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Verb?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Color?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Number?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+
+        System.out.println("What's your favorite Plural?");
+        nounWord1 = keyboard.nextLine();
+        System.out.println("My Favorite Color Is, "+ nounWord1);
+// phase 3
+        System.out.println("A vacation is when you take a trip to some "+ adjectiveWord1 + "place");
+        System.out.println("with your " + adjectiveWord2 + "family. Usually you go to some place");
+        System.out.println("that is near a " + nounWord1 + "or up on a "+ nounWord2);
+        System.out.println("A good vacation place is one where you can ride " + pluralNounWord1);
+        System.out.println("or play " + gameWord1 + "or go hunting for " + pluralNounWord2 + ". I like");
+        System.out.println(" to spend my time " + verbEndingInINGWord1 + "or " + verbEndingInINGWord2 + ".");
+        System.out.println("When parents go on a vacation, they spend their time eating");
+        System.out.println("three " + pluralNounWord3 + "a day, and fathers play golf, and mothers");
+        System.out.println("sit around " + verbEndingInINGWord3 + ". Last summer, my little brother");
+        System.out.println("fell in a " + nounWord3 + "and got poison " + plantWord + "all");
+        System.out.println("over his " + partOfTheBodyWord + ". My family is going to go to the");
+        System.out.println(aPlaceWord + ", and I will practice " + verbEndingInINGWord4 + ". Parents");
+        System.out.println("need vacations more than kids because parents are always very");
+        System.out.println(adjectiveWord3 + "and because they have to work " + numberWord);
+        System.out.println("hours every day all year making enough " + pluralNounWord4 + "to pay");
+        System.out.println("for the vacation.");
+    }
+}
